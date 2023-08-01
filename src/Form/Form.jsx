9cdb9f551cbeed95eebad5b6d72b39fe16babf56
serega5280 <budylin.sergey@gmail.com>
2023-08-01 20:33:01 +0300

    init2

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,27 +1,42 @@
-import React, {useEffect, useState} from "react";
-import './Form.css'
-import { useTelegram } from "../components/hooks/useTelegram";
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-    const [country, setCountry] = useState('')
-    const [street, setStreet] = useState('')
-    const [subject, setSubject] = useState('')
-    const {tg} = useTelegram()
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
-    }, [tg])
+    }, [])
 
     useEffect(() => {
-        if (!street || !country) {
-            tg.MainButton.hide()
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
         }
-        else {
-            tg.MainButton.show() 
-        }
-    }, [country, street, tg.MainButton])
+    }, [country, street])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
@@ -30,32 +45,34 @@ const Form = () => {
     const onChangeStreet = (e) => {
         setStreet(e.target.value)
     }
-    
+
     const onChangeSubject = (e) => {
         setSubject(e.target.value)
     }
 
     return (
-        <div className="Form">
+        <div className={"form"}>
             <h3>Введите ваши данные</h3>
-            <input className="input"
-             type="input"
-             placeholder={"Страна"}
-             value={country}
-             onChange={onChangeCountry}
-             />
-            <input className="input"
-            type="input" 
-            placeholder={"Улица"}
-            value={street}
-            onChange={onChangeStreet}
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
             />
-            <select className={'select'} value={subject} onChange={onChangeSubject}>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
         </div>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
