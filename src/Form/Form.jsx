01e1568d6a5bc35917c87430873ebfaff5a3794a
serega5280 <budylin.sergey@gmail.com>
2023-08-01 20:17:18 +0300

    init1

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -21,7 +21,7 @@ const Form = () => {
         else {
             tg.MainButton.show() 
         }
-    }, [country, street])
+    }, [country, street, tg.MainButton])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
